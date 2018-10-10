96d40e1b85c4f7d49ea98dec6b7592d0f72ceb75
Lyounessi <boukroun.younes@gmail.com>
2018-10-10 01:40:18 +0100

    Adding constant

---
+++ constant.py
@@ -0,0 +1,14 @@
+"""Here Is All constant indeed in the project"""
+
+"""TABLES"""
+
+T_CAT = "categories"
+T_PRODS = "products"
+T_FAV = "favorits"
+
+"""API URLS"""
+
+BOISSON_URL = "https://fr.openfoodfacts.org/categorie/boissons.json"
+FROMAGE_URL = "https://fr.openfoodfacts.org/categorie/fromages.json"
+PROD_TARTINER_URL = "https://fr.openfoodfacts.org/categorie/charcuteries.json"
+CHARCUTERIE_URL = "https://fr.openfoodfacts.org/categorie/produits-a-tartiner.json"
